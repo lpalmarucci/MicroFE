c4c8a28ea18181e44423da6b6bd64232b04cf2ac
Luca Palmarucci <lpalmarucci@gmail.com>
2023-07-16 20:10:00 +0000

    [PDP] Product detail page

--- pdp/src/components/ProductDetail.tsx
+++ pdp/src/components/ProductDetail.tsx
@@ -1,51 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "@mui/material";
-import Button from "@mui/material/Button/Button";
-import CardActions from "@mui/material/CardActions/CardActions";
-import CardContent from "@mui/material/CardContent/CardContent";
-import CardMedia from "@mui/material/CardMedia/CardMedia";
-import Typography from "@mui/material/Typography/Typography";
 import { Product } from "home/models/Product";
 import ProductService from "home/services/ProductService";
 import CircularProgress from "@mui/material/CircularProgress/CircularProgress";
+import { useParams } from "react-router-dom";
+import SplashScreen from "home/components/SplashScreen";
+import PageNotFound from "home/components/NotFound";
 
 type Props = {};
 
 const ProductDetail = (props: Props) => {
-  const productId: number = 1;
+  const { id: productId } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product>();
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    ProductService.getProductById(productId)
+    if (!productId) return;
+    ProductService.getProductById(+productId)
       .then((product) => setProduct(product))
       .finally(() => setLoading(false));
   }, []);
-
   if (loading) return <CircularProgress size={10} />;
 
   return (
-    <div className="min-w-full min-h-full flex justify-center items-center">
-      <Card sx={{ maxWidth: 345, maxHeight: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={product?.image}
-          title={product?.name}
-          src={product?.image}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {product?.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {product?.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+    <div className="grid grid-cols-2 gap-5">
+      <div>
+        <img src={product.image} />
+      </div>
+      <div>
+        <div className="flex text-black py-5">
+          <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
+          <h3 className="font-bold text-3xl flex-end">
+            {ProductService.formatCurrency.format(product.price)}
+          </h3>
+        </div>
+        <div className="mt-10 text-black text-2xl">{product.description}</div>
+        <div className="mt-10 text-md text-gray-500">
+          {product.longDescription}
+        </div>
+      </div>
     </div>
   );
 };
